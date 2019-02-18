6aedcdd1aad7500333041b98f017add2074f3a1c
Guilherme Deusdará <guibanci@gmail.com>
2019-02-18 15:55:58 -0300

    Concluindo algorítmo de injeção dos links

--- nbs-linker.js
+++ nbs-linker.js
@@ -1,10 +1,29 @@
+$.getJSON("dict.json", function(data) {
+    
+    $( document ).ready(function() {
+        var str = document.getElementById("demo").innerHTML; 
+        console.log( "ready!" );
 
-$( document ).ready(function() {
-    console.log( "ready!" );
-    var str = document.getElementById("demo").innerHTML; 
-    var res = str.replace(/[0-9]{1}\.[0-9]{2}([0-9]{2})?(\.[0-9])?([0-9])?(\.[0-9]{2})?/gi, function (x) {
-        x = "<a href=\"#\">" + x + "</a>";
-        return x;
+        var res = str.replace(/[0-9]{1}\.[0-9]{2}([0-9]{2})?(\.[0-9])?([0-9])?(\.[0-9]{2})?/gi, function (x) {
+            var cod = x;
+            
+            //Caso em que x é parte de um código existente. É necessário derivar o código real
+            if (typeof data[cod] == 'undefined') {
+                if (cod.length == 9) cod = cod + ".00";
+                else if (cod.length == 6) cod = cod + ".00.00";                
+            }
+            
+            //Caso o código realmente não existe
+            if (typeof data[cod] == 'undefined') {
+                return x;
+            }
+            
+            var href = "<a href=\"/concepts/" + data[cod][1] + "\" title=\"" + data[cod][0] + "\">" + x + "</a>";
+            return href;
+        });
+
+        document.getElementById("demo").innerHTML = res;
     });
-    document.getElementById("demo").innerHTML = res;
+
 });
+
